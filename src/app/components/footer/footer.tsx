87d9443b06203b7e3594aa2bb532194b92ba8980
Thiago Sales <thiago_salests@hotmail.com>
2024-02-19 11:56:53 -0300

    footer fix name

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
 
+import {info} from '../../database.json';
+
 export default function Footer(){
     return (
         <footer className="text-center p-2 flex items-center justify-center text-stone-500 flex-wrap">
-            Enxoval do Mathias &copy; 2024 - 
+            {info.title} &copy; 2024 - 
                 <Link
                 href={"https://github.com/tsalesproductions/lista-do-mathias/"} target="_blank" className="ml-1 flex gap-2 underline">
                     <Image 
